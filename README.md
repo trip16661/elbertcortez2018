6b019cfe79cdef626f2c82e9a701bbbf0ba719e0
Elbert Cortez <elbertraudry@gmail.com>
2018-04-18 23:33:51 -0500

    website build init

--- README.md
+++
@@ -1,2 +0,0 @@
-# HTML Boilerplate
-Simple HTML Boilerplate using webpack
--- assets/scripts/bundle.js
+++
@@ -1,4 +0,0 @@
-import 'font-awesome/css/font-awesome.css';
-import '../styles/main.scss';
-import './nav';
-import './smoothScroll';
